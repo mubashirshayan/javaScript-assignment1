7f62590aded2d32929f92df604d7e50927bb5667
mubashirshayan <121729482+mubashirshayan@users.noreply.github.com>
2023-04-19 18:36:28 +0500

    New files

---
+++ app.js
@@ -0,0 +1,21 @@
+//JavaScript Assignment No1
+//Question no1
+/*Write a script to greet your website visitor using JS alertbox.*/
+//alert();
+//Question no2
+/*Write a script to display following message on your web page:*/
+//alert('Error! Please enter a valid password')
+//Question no3
+/*Write a script to display following message on your web page: (Hint : Use line break)*/
+//alert('Welcome to Js land'+'<br>'+ 'Happy coading');
+//Question no4
+/*Write a script to display following messages in sequence:*/
+/*alert('Welcome to Js land Happy coading');
+alert('Happy coading');*/
+//Question no5
+/*Generate the following message through browser’s developer console:*/
+/*alert('Hello...I can ran Js through my web browser console');*/
+//Question no6
+/*Make use of alerts in your new/existing HTML & CSS project.*/
+//window.alert('Alert!')
+
